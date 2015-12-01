6af6ed517a5b477012cbbc3e9cafda3cf5794e37
jaydy <jaydy@qb.loni.org>
2015-11-30 21:15:48 -0600

    restart the unfinished task and continue at the stopping smile
    	modified:   src/edud.py

--- src/edud.py
+++ src/edud.py
@@ -70,36 +70,56 @@ class SAscore4Smi(luigi.Task):
     def output(self):
         pass
 
-    def runSA(self, ifn, ofn, error_ofn):
+    def runSA(self, ifn, ofn, error_ofn, restart=True, startline=0):
         import sys
         sys.stderr = open('/dev/null', 'w')  # silence the warnings
 
-        err_ofs = open(error_ofn, 'w')
-        with open(ofn, 'w') as ofs:
-            with open(ifn, 'r') as ifs:
-                smis = [_.rstrip() for _ in ifs]
-                for smi in smis:
-                    try:
-                        m = Chem.MolFromSmiles(smi)
-                        sa = sascorer.calculateScore(m)
-                        ofs.write("%s\t%f\n" % (smi, sa))
-                    except Exception as inst:
-                        err_ofs.write("%s\t%s\n" % (smi, inst.args))
-        err_ofs.close()
+        if restart is True:
+            err_ofs = open(error_ofn, 'w')
+            with open(ofn, 'w') as ofs:
+                with open(ifn, 'r') as ifs:
+                    smis = [_.rstrip() for _ in ifs]
+                    for smi in smis:
+                        try:
+                            m = Chem.MolFromSmiles(smi)
+                            sa = sascorer.calculateScore(m)
+                            ofs.write("%s\t%f\n" % (smi, sa))
+                        except Exception as inst:
+                            err_ofs.write("%s\t%s\n" % (smi, inst.args))
+            err_ofs.close()
+        else:
+            err_ofs = open(error_ofn, 'a')
+            with open(ofn, 'a') as ofs:
+                with open(ifn, 'r') as ifs:
+                    smis = [_.rstrip() for _ in ifs]
+                    for smi in smis[startline:]:
+                        try:
+                            m = Chem.MolFromSmiles(smi)
+                            sa = sascorer.calculateScore(m)
+                            ofs.write("%s\t%f\n" % (smi, sa))
+                        except Exception as inst:
+                            err_ofs.write("%s\t%s\n" % (smi, inst.args))
+            err_ofs.close()
 
     def run(self):
         out_fn = self.smi_path + '.sa.txt'
         error_ofn = self.smi_path + '.sa.error'
 
         if not os.path.exists(out_fn):
-            self.runSA(self.smi_path, out_fn, error_ofn)
+            print("start %s" % out_fn)
+            self.runSA(self.smi_path, out_fn, error_ofn, restart=True)
         else:
             if os.path.exists(error_ofn):
-                total_lines = len(open(self.smi_path, 'r').read())
-                out_lines = len(open(out_fn, 'r').read())
-                error_lines = len(open(error_ofn, 'r').read())
-                if out_lines + error_lines != total_lines:
-                    self.runSA(self.smi_path, out_fn, error_ofn)
+                total_lines = len(open(self.smi_path, 'r').readlines())
+                out_lines = len(open(out_fn, 'r').readlines())
+                error_lines = len(open(error_ofn, 'r').readlines())
+                finished_lines = out_lines + error_lines
+                if finished_lines != total_lines:
+                    print("restart %s ant continue at %d" % (self.smi_path,
+                                                             finished_lines))
+                    self.runSA(self.smi_path, out_fn, error_ofn,
+                               restart=False,
+                               startline=finished_lines)
 
 
 def main(smi_path):
